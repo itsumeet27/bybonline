10f8c399e150336ee6666dd3ab28240bfbcabee9
Sumeet Sharma <sksksharma0@gmail.com>
2018-07-23 16:14:41 +0530

    Add files via upload

---
+++ cake.php
@@ -0,0 +1,428 @@
+<?php
+    include('includes/header.php');
+?>
+
+<head>
+	<!-- Custom CSS -->
+        <link rel="stylesheet" type="text/css" href="css/custom.css">
+        <style type="text/css">
+			 .hovereffect {
+			width: 100%;
+			height: 100%;
+			float: left;
+			overflow: hidden;
+			position: relative;
+			text-align: center;
+			cursor: default;
+			}
+
+			.hovereffect .overlay {
+			position: absolute;
+			overflow: hidden;
+			width: 80%;
+			height: 80%;
+			left: 10%;
+			top: 10%;
+			border-bottom: 1px solid #FFF;
+			border-top: 1px solid #FFF;
+			-webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+			transition: opacity 0.35s, transform 0.35s;
+			-webkit-transform: scale(0,1);
+			-ms-transform: scale(0,1);
+			transform: scale(0,1);
+			}
+
+			.hovereffect:hover .overlay {
+			opacity: 1;
+			filter: alpha(opacity=100);
+			-webkit-transform: scale(1);
+			-ms-transform: scale(1);
+			transform: scale(1);
+			}
+
+			.hovereffect img {
+			display: block;
+			position: relative;
+			-webkit-transition: all 0.35s;
+			transition: all 0.35s;
+			}
+
+			.hovereffect:hover img {
+			filter: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feComponentTransfer color-interpolation-filters="sRGB"><feFuncR type="linear" slope="0.6" /><feFuncG type="linear" slope="0.6" /><feFuncB type="linear" slope="0.6" /></feComponentTransfer></filter></svg>#filter');
+			filter: brightness(0.6);
+			-webkit-filter: brightness(0.6);
+			}
+
+			.hovereffect h2 {
+			text-transform: uppercase;
+			text-align: center;
+			position: relative;
+			font-size: 17px;
+			background-color: transparent;
+			color: #FFF;
+			padding: 1em 0;
+			opacity: 0;
+			filter: alpha(opacity=0);
+			-webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+			transition: opacity 0.35s, transform 0.35s;
+			-webkit-transform: translate3d(0,-100%,0);
+			transform: translate3d(0,-100%,0);
+			}
+
+			.hovereffect a, .hovereffect p {
+			color: #FFF;
+			padding: 1em 0;
+			opacity: 0;
+			filter: alpha(opacity=0);
+			-webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+			transition: opacity 0.35s, transform 0.35s;
+			-webkit-transform: translate3d(0,100%,0);
+			transform: translate3d(0,100%,0);
+			}
+
+			.hovereffect:hover a, .hovereffect:hover p, .hovereffect:hover h2 {
+			opacity: 1;
+			filter: alpha(opacity=100);
+			-webkit-transform: translate3d(0,0,0);
+			transform: translate3d(0,0,0);
+			}
+
+			.table td, .table>tbody>tr>td, .table>tbody>tr>th, .table>tfoot>tr>td, .table>tfoot>tr>th, .table>thead>tr>td, .table>thead>tr>th {
+				  	padding: 5px!important;
+				}
+				.table > thead > tr > th, .table > tbody > tr > th, .table > tfoot > tr > th, .table > thead > tr > td, .table > tbody > tr > td, .table > tfoot > tr > td {
+				  	font-size: 0.75em;
+				  	color: #f9f9f9;
+				  	border-top: none !important;
+				  	font-family: Raleway;
+				}
+
+				.table tr{
+
+				}
+
+				.buy{
+					text-align: center;
+					margin: 10%;
+				}
+
+				.buy a{
+					padding: 15px 27px;
+					font-size: 18px;
+					font-family: Raleway;
+					color: #fff;
+					font-weight: bold;
+					background-color: #654321;
+					border: 1px solid #fff;
+				}
+
+				.buy a:hover{
+					color: #654321;
+					transition-duration: 0.4s;
+					background-color: #fff;
+					border: 1px solid #654321;
+				}
+
+				.col-md-2 input[type='submit'], .col-md-2 input[type='reset']{
+					padding: 7px 15px;
+					color: #fff;
+					font-size: 16px;
+					background-color: #555;
+					border: 1px solid #fff;
+					width: 100%;
+				}
+
+				.col-md-2 input[type='submit']:hover, .col-md-2 input[type='reset']:hover{
+					color: #555;
+					background-color: #fff;
+					border: 1px solid #555;
+					transition-duration: 0.3s;
+				}
+
+				#filters .col-md-2 label{
+					font-size: 16px;
+					font-family: Raleway;
+				}
+	        </style> 
+	        <script type="text/javascript">
+	        	function show_value(x)
+				{
+				 	document.getElementById("slider_value").innerHTML=x;
+				}
+	        </script>
+</head>
+
+	<hr>
+		<h1 align="center" id="user-head">Cake</h1>
+	<hr>
+	<div class="row">
+		<div class="container">
+			<div id="filters">
+				<div class="col-md-2">
+					<h3>Filters: </h3>
+				</div>
+				<div class="col-md-2">
+					<label>Shape: </label><br>
+					<input type="checkbox" name="shape" value="rectangle">&nbsp;Rectangle <br>
+					<input type="checkbox" name="shape" value="square">&nbsp;Square <br>
+					<input type="checkbox" name="shape" value="circle">&nbsp;Circle <br>
+					<input type="checkbox" name="shape" value="heart">&nbsp;Heart <br>
+				</div>
+				<div class="col-md-2">
+					<label>Flavor: </label><br>
+					<input type="checkbox" name="flavor" value="blackforest">&nbsp;Black forest <br>
+					<input type="checkbox" name="flavor" value="pineapple">&nbsp;Pineapple <br>
+					<input type="checkbox" name="flavor" value="redvelvet">&nbsp;Red velvet <br>
+					<input type="checkbox" name="flavor" value="fruit">&nbsp;Fruit cake <br>
+				</div>
+				<div class="col-md-2">
+					<label>Price range: </label><br>
+					<span id="slider_value" style="color:black;text-align: center">0</span>
+					<input width="400" type="range" min="0" max="1499" name="sld3" value="499" onchange="show_value(this.value);">1499
+				</div>
+				<div class="col-md-2">
+					<label>Weight: </label><br>
+					<input type="checkbox" name="weight" value="0.5">&nbsp;0.5 kg <br>
+					<input type="checkbox" name="weight" value="1">&nbsp;1 kg <br>
+					<input type="checkbox" name="weight" value="2">&nbsp;2 kg <br>
+					<input type="checkbox" name="weight" value="2.5">&nbsp;2.5 kg <br>
+				</div>
+				<div class="col-md-2">
+					<input type="submit" name="submit" value="Submit"><br>
+					<input type="reset" name="reset" value="Reset">
+				</div>
+			</div>
+		</div><br>
+		<div class="container">
+			<div class="col-md-4">
+				<div width="300">
+				    <div class="hovereffect">
+				        <img class="img-responsive" src="images/cake2.jpg" alt="">
+			            <div class="overlay">
+			                <h2>Cake</h2>
+							<table class="table">
+								<tr>
+									<th>Weight:</th>
+									<td>0.5 kg</td>
+								</tr>
+								<tr>
+									<th>Price:</th>
+									<td>&#8377; 350</td>
+								</tr>
+								<tr>
+									<th>Shape:</th>
+									<td>Round</td>
+								</tr>
+								<tr>
+									<th>Sugar free:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Veg:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Ingredient:</th>
+									<td>Value</td>
+								</tr>
+							</table>
+							<a href="#">Add to Cart</a>
+			            </div>
+				    </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div width="300">
+				    <div class="hovereffect">
+				        <img class="img-responsive" src="images/cake2.jpg" alt="">
+			            <div class="overlay">
+			                <h2>Cake</h2>
+							<table class="table">
+								<tr>
+									<th>Weight:</th>
+									<td>0.5 kg</td>
+								</tr>
+								<tr>
+									<th>Price:</th>
+									<td>&#8377; 350</td>
+								</tr>
+								<tr>
+									<th>Shape:</th>
+									<td>Round</td>
+								</tr>
+								<tr>
+									<th>Sugar free:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Veg:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Ingredient:</th>
+									<td>Value</td>
+								</tr>
+							</table>
+							<a href="#">Add to Cart</a>
+			            </div>
+				    </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div width="300">
+				    <div class="hovereffect">
+				        <img class="img-responsive" src="images/cake2.jpg" alt="">
+			            <div class="overlay">
+			                <h2>Cake</h2>						
+							<table class="table">
+								<tr>
+									<th>Weight:</th>
+									<td>0.5 kg</td>
+								</tr>
+								<tr>
+									<th>Price:</th>
+									<td>&#8377; 350</td>
+								</tr>
+								<tr>
+									<th>Shape:</th>
+									<td>Round</td>
+								</tr>
+								<tr>
+									<th>Sugar free:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Veg:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Ingredient:</th>
+									<td>Value</td>
+								</tr>
+							</table>
+							<a href="#">Add to Cart</a>						
+			            </div>			     
+				    </div>
+				</div>
+			</div>
+		</div>
+		<div><br></div>
+		<div class="container">
+			<div class="col-md-4">
+				<div width="300">
+				    <div class="hovereffect">
+				        <img class="img-responsive" src="images/cake2.jpg" alt="">
+			            <div class="overlay">
+			                <h2>Cake</h2>
+							<table class="table">
+								<tr>
+									<th>Weight:</th>
+									<td>0.5 kg</td>
+								</tr>
+								<tr>
+									<th>Price:</th>
+									<td>&#8377; 350</td>
+								</tr>
+								<tr>
+									<th>Shape:</th>
+									<td>Round</td>
+								</tr>
+								<tr>
+									<th>Sugar free:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Veg:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Ingredient:</th>
+									<td>Value</td>
+								</tr>
+							</table>
+							<a href="#">Add to Cart</a>
+			            </div>
+				    </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div width="300">
+				    <div class="hovereffect">
+				        <img class="img-responsive" src="images/cake2.jpg" alt="">
+			            <div class="overlay">
+			                <h2>Cake</h2>
+							<table class="table">
+								<tr>
+									<th>Weight:</th>
+									<td>0.5 kg</td>
+								</tr>
+								<tr>
+									<th>Price:</th>
+									<td>&#8377; 350</td>
+								</tr>
+								<tr>
+									<th>Shape:</th>
+									<td>Round</td>
+								</tr>
+								<tr>
+									<th>Sugar free:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Veg:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Ingredient:</th>
+									<td>Value</td>
+								</tr>
+							</table>
+							<a href="#">Add to Cart</a>
+			            </div>
+				    </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div width="300">
+				    <div class="hovereffect">
+				        <img class="img-responsive" src="images/cake2.jpg" alt="">
+			            <div class="overlay">
+			                <h2>Cake</h2>						
+							<table class="table">
+								<tr>
+									<th>Weight:</th>
+									<td>0.5 kg</td>
+								</tr>
+								<tr>
+									<th>Price:</th>
+									<td>&#8377; 350</td>
+								</tr>
+								<tr>
+									<th>Shape:</th>
+									<td>Round</td>
+								</tr>
+								<tr>
+									<th>Sugar free:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Veg:</th>
+									<td>Yes</td>
+								</tr>
+								<tr>
+									<th>Ingredient:</th>
+									<td>Value</td>
+								</tr>
+							</table>
+							<a href="#">Add to Cart</a>						
+			            </div>			     
+				    </div>
+				</div>
+			</div>
+		</div>
+		<div><br></div>
+	</div>
+
+<?php
+    include('includes/footer.php');
+?>
---
+++ products.php
@@ -0,0 +1,104 @@
+<?php
+    include('includes/header.php');
+?>
+
+<head>
+	<!-- Custom CSS -->
+        <link href="css/custom.css" rel="stylesheet">  
+</head>
+
+<!--Product Page-->
+
+<div id="category-nav">
+	<hr>
+		<h1 align="center" id="user-head">Our Product Categories</h1>
+	<hr>
+	<div class="banner-bottom animated wow lightSpeedIn" data-wow-duration="1500ms" data-wow-delay="800ms">
+		<ul id="flexiselDemo1">	
+			<li>
+				<a href="cake.php"><div class="item item-sub">
+					<div class="item1">
+						<i class="glyphicon glyphicon-cutlery" aria-hidden="true"></i>
+						<h3>Cake</h3>
+					</div>
+					<div class="p-mask">
+						<h4>Bake Your Bread</h4>
+						<p>Content.</p>
+						
+					</div>
+				</div></a>
+			</li>
+			<li>
+				<a href="bread.php"><div class="item item-sub1">
+					<div class="item1">
+						<i class="glyphicon glyphicon-cutlery" aria-hidden="true"></i>
+						<h3>Bread</h3>
+					</div>
+					<div class="p-mask">
+						<h4>Bake Your Bread</h4>
+						<p>Content</p>
+					</div>
+				</div></a>
+			</li>
+			<li>
+				<a href="cookies.php"><div class="item item-sub2">
+					<div class="item1">
+						<i class="glyphicon glyphicon-cutlery" aria-hidden="true"></i>
+						<h3>Cookies</h3>
+					</div>
+					<div class="p-mask">
+						<h4>Bake Your Bread</h4>
+						<p>Content.</p>
+					</div>
+				</div></a>
+			</li>
+			<li>
+				<a href="pastries.php"><div class="item item-sub3">
+					<div class="item1">
+						<i class="glyphicon glyphicon-cutlery" aria-hidden="true"></i>
+						<h3>Pastries</h3>
+					</div>
+					<div class="p-mask">
+						<h4>Bake Your Bread</h4>
+						<p>Content.</p>
+					</div>
+				</div></a>
+			</li>
+		</ul>
+			<script type="text/javascript">
+				$(window).load(function() {
+				$("#flexiselDemo1").flexisel({
+					visibleItems: 4,
+					animationSpeed: 1000,
+					autoPlay: true,
+					autoPlaySpeed: 3000,    		
+					pauseOnHover: true,
+					enableResponsiveBreakpoints: true,
+					responsiveBreakpoints: { 
+						portrait: { 
+							changePoint:480,
+							visibleItems: 1
+						}, 
+						landscape: { 
+							changePoint:640,
+							visibleItems: 2
+						},
+						tablet: { 
+							changePoint:768,
+							visibleItems: 3
+						}
+					}
+				});
+				
+			 });
+			</script>
+			<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+	</div>
+<!-- //banner-bottom -->
+</div>
+
+<!--/Product Page-->
+
+<?php
+    include('includes/footer.php');
+?>
